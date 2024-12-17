1826c462d79de5c251fd5ed462cf56c3c35d4c8e
Hailemichael Atrsaw Tibebu <hailat.alx@gmail.com>
2024-12-17 20:51:08 +0300

    feat: stock movemnt add/subs completed

--- warehouse/serializers/stock_movements_serializer.py
+++ warehouse/serializers/stock_movements_serializer.py
@@ -36,18 +36,38 @@ class StockMovementsSerializer(ModelSerializer):
         )
         stock.quantity < attrs.get("quantity")
         min_stock_level = stock.product.min_stock_level
-        if min_stock_level >= (stock.quantity - attrs.get("quantity")):
-            raise ValidationError(
-                {
-                    "min_stock_level": (
-                        f"The allowed quantity to move must be at most "
-                        f"({stock.quantity - min_stock_level})."
-                    )
-                }
-            )
+        if min_stock_level > (stock.quantity - attrs.get("quantity")):
+            remaining_allowed = stock.quantity - min_stock_level
+            if remaining_allowed <= 0:
+                raise ValidationError(
+                    {
+                        "quantity": (
+                            f"You cannot move that many. The maximum quantity you can move is {stock.quantity} to maintain the minimum stock level of {min_stock_level}."  # noqa E501
+                            if remaining_allowed < 0
+                            else f"Moving this quantity would bring the stock level below the minimum of {min_stock_level}."  # noqa E501
+                        )
+                    }
+                )
+            else:
+                raise ValidationError(
+                    {
+                        "quantity": (
+                            f"You cannot move that many. You can move at most {remaining_allowed} to maintain the minimum stock level of {min_stock_level}."  # noqa E501
+                        )
+                    }
+                )
 
         stock.quantity -= attrs.get("quantity")
         stock.save()
+
+        dest_stock = get_object_or_404(
+            Stocks,
+            warehouse=attrs.get("destination_warehouse"),
+            product=attrs.get("product"),
+        )
+
+        dest_stock.quantity += attrs.get("quantity")
+        dest_stock.save()
 
         return super().validate(attrs)
 
