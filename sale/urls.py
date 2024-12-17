78a02e141dc19b08cd4bc239724ac8956b264c33
Hailemichael Atrsaw Tibebu <hailat.alx@gmail.com>
2024-12-17 22:51:04 +0300

    feat: serializer and url fix

---
+++ sale/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from sale.views import SalesAPIView
+
+urlpatterns = [
+    path("sales/", SalesAPIView.as_view(), name="sale-create-list"),
+    path("sales/<uuid:pk>", SalesAPIView.as_view(), name="sale-detail-patch"),
+]
